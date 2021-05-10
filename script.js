7427b2eacae60bb584ef0610a35b7af9f0db7ea9
Максим Колтович <12345kolt@gmail.com>
2021-05-10 16:47:17 +0300

    AJAX-запросы

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
- const btn = document.querySelector('button');
-
+const btn = document.querySelector('button');
+const container = document.querySelector('.container');
 
 function getPosts(cb) {
-
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", 'http://api.weatherstack.com/current?access_key=a54d1451af691342e975fde0186820bf&query=Moscow');
+    xhr.open("GET", "http://jsonplaceholder.typicode.com/posts");
     xhr.addEventListener("load", () => {
         const response = JSON.parse(xhr.responseText);
         cb(response);
@@ -12,14 +11,32 @@ function getPosts(cb) {
 
     xhr.addEventListener("error", () => {
         console.log('error');
-    })
+    });
 
     xhr.send();
 }
+function renderPosts(response) {
+    const fragment = document.createDocumentFragment();
+    response.forEach(post => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        const title = document.createElement('h5');
+        title.classList.add('card-title');
+        title.textContent = post.title;
+        const article = document.createElement('p');
+        article.classList.add('card-text');
+        article.textContent = post.body;
+        cardBody.appendChild(title);
+        cardBody.appendChild(article);
+        card.appendChild(cardBody);
+        fragment.appendChild(card);
+    });
+    container.appendChild(fragment);
+}
 
 btn.addEventListener('click', (e) => {
-    getPosts((response) => {
-        console.log(response);
-    });
+    getPosts(renderPosts);
 });
 
