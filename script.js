7867ef21b0165aa480a5e4b75b46de2ab2902dae
Максим Колтович <12345kolt@gmail.com>
2021-05-10 15:31:07 +0300

    AJAX-запросы

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const xhr = new XMLHttpRequest();
-xhr.open("GET", 'http://api.weatherstack.com/current?access_key=a54d1451af691342e975fde0186820bf&query=Moscow');
-xhr.addEventListener("load", () => {
-    console.log(xhr.responseText);
+ const btn = document.querySelector('button');
+
+
+function getPosts(cb) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", 'http://api.weatherstack.com/current?access_key=a54d1451af691342e975fde0186820bf&query=Moscow');
+    xhr.addEventListener("load", () => {
+        const response = JSON.parse(xhr.responseText);
+        cb(response);
+    });
+
+    xhr.addEventListener("error", () => {
+        console.log('error');
+    })
+
+    xhr.send();
+}
+
+btn.addEventListener('click', (e) => {
+    getPosts((response) => {
+        console.log(response);
+    });
 });
 
-xhr.addEventListener("error", () => {
-    console.log('error');
-})
-
-xhr.send();
