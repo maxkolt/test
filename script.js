2a7b18fe659a18f2f4a5fc948cef647b0a396e8b
Максим Колтович <12345kolt@gmail.com>
2021-05-10 16:59:39 +0300

    AJAX-запросы

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.querySelector('.container');
 
 function getPosts(cb) {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://jsonplaceholder.typicode.com/posts");
+    xhr.open("GET", "http://api.weatherstack.com/current?access_key=a54d1451af691342e975fde0186820bf&query=Moscow");
     xhr.addEventListener("load", () => {
         const response = JSON.parse(xhr.responseText);
         cb(response);
