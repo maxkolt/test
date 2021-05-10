71e8fb5f9ae5b9fd1607d37a137d1a8ae3b2e483
Максим Колтович <12345kolt@gmail.com>
2021-05-10 15:08:27 +0300

    AJAX

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ xhr.open("GET", "http://jsonplaceholder.typicode.com/posts");
 xhr.addEventListener("load", () => {
     console.log(xhr.responseText);
 });
+
+xhr.addEventListener("error", () => {
+    console.log('error');
+})
+
 xhr.send();
