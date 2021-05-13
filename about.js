36886aa7446d366810080e2b72e5f7d5f6d8f595
Максим Колтович <12345kolt@gmail.com>
2021-05-13 23:27:48 +0300

    Маленький проект про погоду на ванильном JS

---
+++ about.js
@@ -0,0 +1,61 @@
+function customHttp() {
+    return {
+        get(url, cb) {
+            try {
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', url);
+                xhr.addEventListener('load', () => {
+                    if (Math.floor(xhr.status / 100) !== 2) {
+                        cb(`Error. Status code: ${xhr.status}`, xhr);
+                        return;
+                    }
+                    const response = JSON.parse(xhr.responseText);
+                    cb(null, response);
+                });
+
+                xhr.addEventListener('error', () => {
+                    cb(`Error. Status code: ${xhr.status}`, xhr);
+                });
+
+                xhr.send();
+            } catch (error) {
+                cb(error);
+            }
+        },
+        post(url, body, headers, cb) {
+            try {
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', url);
+                xhr.addEventListener('load', () => {
+                    if (Math.floor(xhr.status / 100) !== 2) {
+                        cb(`Error. Status code: ${xhr.status}`, xhr);
+                        return;
+                    }
+                    const response = JSON.parse(xhr.responseText);
+                    cb(null, response);
+                });
+
+                xhr.addEventListener('error', () => {
+                    cb(`Error. Status code: ${xhr.status}`, xhr);
+                });
+
+                if (headers) {
+                    Object.entries(headers).forEach(([key, value]) => {
+                        xhr.setRequestHeader(key, value);
+                    });
+                }
+
+                xhr.send(JSON.stringify(body));
+            } catch (error) {
+                cb(error);
+            }
+        },
+    };
+}
+
+const http = customHttp();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+});
