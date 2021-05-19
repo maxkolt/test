3455d7091a630285c55ff3309f46a7ad4b6c8d9c
Максим Колтович <12345kolt@gmail.com>
2021-05-19 14:01:07 +0300

    fetch запросы

--- fetch.js
+++ fetch.js
@@ -1,3 +1,9 @@
-function f() {
-
+function getPost(id) {
+    return new Promise((resolve, reject) => {
+        fetch(`http://api.weatherstack.com/current?access_key=a54d1451af691342e975fde0186820bf&query=Moscow${id}`)
+            .then(response => response.json())
+            .then(post => resolve(post))
+            .catch(err => reject(err))
+    })
 }
+getPost(1).then(post => console.log(post))
